c2dcddc8aa973f44b126bc255bf0b87ab806ca3c
Ubuntu <administrador@vm-dane-modsfc.j2s00u1vvifu1pcmmogchgdneg.cx.internal.cloudapp.net>
2021-10-01 08:26:07 +0000

    Request scraper

--- scraper/facebook/scrap_profiles.py
+++ scraper/facebook/scrap_profiles.py
@@ -7,22 +7,21 @@ import json, time, random
 from fbscrap import FBScraper
 
 DIRECT_DOWNLOAD_URL = 'https://drive.google.com/uc?export=download&id=1kOT1xCZ6NEkEmPaqfpmfoTok30Vqd-2K'
-OUTPUT_DATA = 'profiles_data_'
+OUTPUT_DATA = '/home/administrador/output-facebook/profiles_data_{}.json'
 
 def read_ids():
     df = pd.read_csv(DIRECT_DOWNLOAD_URL, names=['commenter_id'])
+    df = df.astype({'commenter_id' : str})
     print(df.info())
     return df
 
 
 def scrap_profile(com_id):    
-    time.sleep(random.randint(1,5))
+    time.sleep(random.randint(2,5))
     profile = []
-    try:
-        profile = get_profile(com_id) 
-    except exceptions.TemporarilyBanned:
-        print("\nTemporarily Banned!")
-        print("stopped in commenter id {}".format(com_id))
+    
+    profile = get_profile(com_id) 
+    
     return profile
 
 def save_profiles(list_data):
@@ -40,6 +39,19 @@ if __name__ == "__main__":
     if len(fb.scrap_posts()) > 0:
         ids = df_profiles.commenter_id.unique()
         list_profiles = []
-        for id in tqdm(ids):
-            list_profiles.append(scrap_profile(id))
+        iters = 37
+        for id in tqdm(ids[37:]):
+            try:
+                list_profiles.append(scrap_profile(id))
+                iters += 1
+                if iters % 30 == 0:
+                    print('Waiting a long time to escape banning')
+                    time.sleep(random.randint(1800,3600))
+                    print('Resuming scraping...')
+            except exceptions.TemporarilyBanned:
+                print("\nTemporarily Banned!")
+                print("stopped in {} iteration, commenter id {}".format(iters, id))
+                break
+            finally:
+                save_profiles(list_profiles)
     save_profiles(list_profiles)
