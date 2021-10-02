5a56b093419c422260144c5ef3df256edee3b1ba
Andres Jaque <rajaquep@gmail.com>
2021-10-03 07:42:11 +0800

    Updating demo

--- scraper/demo_scrap.py
+++ scraper/demo_scrap.py
@@ -1,11 +1,13 @@
 from facebook_bot.FbBot import FbBot
 import facebook_bot.FbScraper as fbs
 
-fb = FbBot()
+fb = FbBot(transformer=False)
 user = fb.FB_account_opener('leodatom','Moscow$101')
 #fb.posting_bot(user)
 
-#profile_data = fbs.profile_scraper(user, 'https://m.facebook.com/AndreaOchoaL97/about?lst=100047716653194%3A1421843452%3A1632837488')
-profile_data = fbs.profile_scraper(user, 'https://m.facebook.com/carloss.nietoo/about')
+profile_data = fbs.profile_scraper(user, 'https://m.facebook.com/AndreaOchoaL97/about?lst=100047716653194%3A1421843452%3A1632837488')
+#profile_data = fbs.profile_scraper(user, 'https://m.facebook.com/carloss.nietoo/about')
 
+fb.scrolling_bot(user)
 
+print(profile_data)
